c45a9b8208422b7cbe973e86159f25e0764f95b5
EGGSY <eggsydev@outlook.com>
2021-05-09 23:15:20 +0300

    fix: add spotify types

--- @types/lanyard.d.ts
+++ @types/lanyard.d.ts
@@ -11,13 +11,27 @@ export interface LanyardResponse {
 }
 
 export interface LanyardData {
-  spotify?: any;
+  spotify?: Spotify;
   listening_to_spotify: boolean;
   discord_user: Discorduser;
   discord_status: string;
   activities: Activity[];
   active_on_discord_mobile: boolean;
   active_on_discord_desktop: boolean;
+}
+
+export interface Spotify {
+  track_id: string;
+  timestamps: Timestamps;
+  song: string;
+  artist: string;
+  album_art_url: string;
+  album: string;
+}
+
+export interface Timestamps {
+  start: number;
+  end: number;
 }
 
 export interface Activity {
